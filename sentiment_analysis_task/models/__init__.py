22c73ecca330faf4053c19e14e8f51e004192718
seokhun <sostar0832@cau.ac.kr>
2025-09-17 15:09:03 +0900

    코드 수정

--- sentiment_analysis_task/models/__init__.py
+++ sentiment_analysis_task/models/__init__.py
@@ -2,6 +2,7 @@ from .lstm_classfication import LSTMClassifier
 
 def build_model(model_name : str,
             vocab_size,
+            num_classes,
             embed_dim,
             hidden_dim,
             num_layers,
@@ -12,6 +13,7 @@ def build_model(model_name : str,
     if model_name == 'lstm_classification':
         model = LSTMClassifier(
             vocab_size=vocab_size,
+            num_classes=num_classes,
             embed_dim=embed_dim,
             hidden_dim=hidden_dim,
             num_layers=num_layers,
