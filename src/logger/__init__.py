02857d524279ed3e8d0571d4499e52f3b649ebad
seokhun <sostar0832@cau.ac.kr>
2025-09-21 00:24:51 +0900

    train.py 수정

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,3 +1,4 @@
 from src.logger.tb_logger import TBLogger
 from src.logger.wandb_logger import WandbLogger
 from src.logger.tqdm_logger import TqdmLogger
+from src.logger.simple_logger import SimpleLogger
