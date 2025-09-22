f24c6cea1f088495cb938ea807968606b0cd35f2
seokhun <sostar0832@cau.ac.kr>
2025-09-22 10:54:25 +0900

    classification 추가

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from src.models.resnet import ResNet_mini
 from src.models.resnetv2 import ResNet_mini_v2_28, ResNet_mini_v2_14x2
 from src.models.lstm_attention import LSTMAttentionClassifier
+from src.models.bert import BertClassifier
