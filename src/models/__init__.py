6584006071845470288f4814d49f30cfa639cc68
seokhun <sostar0832@cau.ac.kr>
2025-09-22 21:54:24 +0900

    lstm 모델 변경

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,5 @@
 from src.models.resnet import ResNet_mini
 from src.models.resnetv2 import ResNet_mini_v2_28, ResNet_mini_v2_14x2
 from src.models.lstm_attention import LSTMAttentionClassifier
+from src.models.lstm import LSTMClassifier
 from src.models.bert import BertClassifier
