09a41b0efcc8a7bdcf0f7a5ac7f71fc6cf05b35b
seokhun <sostar0832@cau.ac.kr>
2025-09-23 10:16:41 +0900

    se resnet 추가

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,4 @@ from src.models.resnetv2 import ResNet_mini_v2_28, ResNet_mini_v2_14x2
 from src.models.lstm_attention import LSTMAttentionClassifier
 from src.models.lstm import LSTMClassifier
 from src.models.bert import BertClassifier
+from src.models.se_resnetv2 import SE_ResNet_mini_v2_28, SE_ResNet_mini_v2_14x2
