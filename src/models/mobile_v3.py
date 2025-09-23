e364ef3998ae091c68624849312b2504289d83d8
seokhun <sostar0832@cau.ac.kr>
2025-09-23 15:54:41 +0900

    mobile resnet 추가

--- src/models/mobile_v3.py
+++ src/models/mobile_v3.py
@@ -1,5 +1,6 @@
 # REF)
 # https://github.com/d-li14/mobilenetv2.pytorch/blob/master/models/imagenet/mobilenetv2.py 
+# https://github.com/d-li14/mobilenetv3.pytorch/blob/master/mobilenetv3.py
 
 from typing import List
 import torch
@@ -10,7 +11,7 @@ from src.models.base import ModelBase
 import math
 
 class SEBlock(nn.Module):
-    def __init__(self, channel, reduction=16):
+    def __init__(self, channel, reduction=4):
         super(SEBlock, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
@@ -48,38 +49,58 @@ class Depthwise(nn.Module):
         x = self.pointwise(x)
         return x
 
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
 
 class InvertedResidual(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, expand_ratio):
+    def __init__(self, inp, hidden_dim, oup, kernel_size, stride, use_se, use_hs):
         super(InvertedResidual, self).__init__()
         assert stride in [1, 2]
 
-        hidden_dim = round(in_channels * expand_ratio)
-        self.identity = stride == 1 and in_channels == out_channels
+        self.identity = stride == 1 and inp == oup
 
-        if expand_ratio == 1:
+        if inp == hidden_dim:
             self.conv = nn.Sequential(
                 # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
                 nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
+                h_swish() if use_hs else nn.ReLU(inplace=True),
+                # Squeeze-and-Excite
+                SEBlock(hidden_dim) if use_se else nn.Identity(),
                 # pw-linear
-                nn.Conv2d(hidden_dim, out_channels, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(out_channels),
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
             )
         else:
             self.conv = nn.Sequential(
                 # pw
-                nn.Conv2d(in_channels, hidden_dim, 1, 1, 0, bias=False),
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
+                h_swish() if use_hs else nn.ReLU(inplace=True),
                 # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, (kernel_size - 1) // 2, groups=hidden_dim, bias=False),
                 nn.BatchNorm2d(hidden_dim),
-                nn.ReLU6(inplace=True),
+                # Squeeze-and-Excite
+                SEBlock(hidden_dim) if use_se else nn.Identity(),
+                h_swish() if use_hs else nn.ReLU(inplace=True),
                 # pw-linear
-                nn.Conv2d(hidden_dim, out_channels, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(out_channels),
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
             )
 
     def forward(self, x):
@@ -89,31 +110,19 @@ class InvertedResidual(nn.Module):
             return self.conv(x)
 
 
-def conv_3x3_bn(in_channels, out_channels, stride):
+def conv_3x3_bn(inp, oup, stride):
     return nn.Sequential(
-        nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=3,
-            stride=stride,
-            padding=1,
-            bias=False
-            ),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU6(inplace=True)
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        h_swish()
     )
 
-def conv_1x1_bn(in_channels, out_channels):
+
+def conv_1x1_bn(inp, oup):
     return nn.Sequential(
-        nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            bias=False),
-        nn.BatchNorm2d(out_channels),
-        nn.ReLU6(inplace=True)
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        h_swish()
     )
 
 
@@ -135,28 +144,37 @@ def _make_divisible(
     return new_v
 
 
-class MobileNetV2_mini(nn.Module):
-    def __init__(self, layers, num_classes=100, width_mult=1.):
-        super(MobileNetV2_mini, self).__init__()
+class MobileNetV3(nn.Module):
+    def __init__(self, cfgs, mode, num_classes=100, width_mult=1.):
+        super(MobileNetV3, self).__init__()
         # setting of inverted residual blocks
-        self.layers = layers
+        self.cfgs = cfgs
+        assert mode in ['large', 'small']
 
         # building first layer
-        input_channel = _make_divisible(32 * width_mult, 4 if width_mult == 0.1 else 8)
-        layers = [conv_3x3_bn(3, input_channel, 1)] # 초기 stride를 1로
+        input_channel = _make_divisible(16 * width_mult, 8)
+        layers = [conv_3x3_bn(3, input_channel, 1)] # stride 1로 수정
+
 
         # building inverted residual blocks
-        for t, c, n, s in self.layers:
-            output_channel = _make_divisible(c * width_mult, 4 if width_mult == 0.1 else 8)
-            for i in range(n):
-                layers.append(InvertedResidual(input_channel, output_channel, s if i == 0 else 1, t))
-                input_channel = output_channel
+        block = InvertedResidual
+        for k, t, c, use_se, use_hs, s in self.cfgs:
+            output_channel = _make_divisible(c * width_mult, 8)
+            exp_size = _make_divisible(input_channel * t, 8)
+            layers.append(block(input_channel, exp_size, output_channel, k, s, use_se, use_hs))
+            input_channel = output_channel
         self.features = nn.Sequential(*layers)
         # building last several layers
-        output_channel = _make_divisible(1280 * width_mult, 4 if width_mult == 0.1 else 8) if width_mult > 1.0 else 1280
-        self.conv = conv_1x1_bn(input_channel, output_channel)
+        self.conv = conv_1x1_bn(input_channel, exp_size)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Linear(output_channel, num_classes)
+        output_channel = {'large': 1280, 'small': 1024}
+        output_channel = _make_divisible(output_channel[mode] * width_mult, 8) if width_mult > 1.0 else output_channel[mode]
+        self.classifier = nn.Sequential(
+            nn.Linear(exp_size, output_channel),
+            h_swish(),
+            nn.Dropout(0.2),
+            nn.Linear(output_channel, num_classes),
+        )
 
         self._initialize_weights()
 
@@ -182,18 +200,22 @@ class MobileNetV2_mini(nn.Module):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
 
-@register("model", "mobilenet_v2_mini")
-class mobilenet_v2_mini(MobileNetV2_mini, ModelBase):
+@register("model", "mobilenet_v3_mini")
+class mobilenet_v3_mini(MobileNetV3, ModelBase):
     def __init__(self, **kwargs):
         # Call the parent class's constructor with the fixed layers
-        layers = [
-            # t, c, n, s
-            [1,  16, 1, 1], # (32,32, 16)
-            [6,  24, 2, 2], # (16,16, 24)
-            [6,  32, 3, 1], # (16,16, 32)
-            [6,  64, 4, 1], # (16,16, 64)
-            [6,  96, 3, 2], # ( 8, 8, 96)
-            [6, 160, 3, 1], # ( 8, 8,160)
-            [6, 320, 1, 1], # ( 8, 8,320)
+        cfgs = [
+            # k, t, c, SE, HS, s 
+            # [3,    1,  16, 1, 0, 2], 
+            # [3,  4.5,  24, 0, 0, 2],
+            [3, 3.67,  24, 0, 0, 1], # exp size=88(16*3.67), out_ch=16, SE=false, NL=Relu, stride=1
+            [5,    4,  40, 1, 1, 2],
+            [5,    6,  40, 1, 1, 1],
+            [5,    6,  40, 1, 1, 1],
+            [5,    3,  48, 1, 1, 1],
+            [5,    3,  48, 1, 1, 1],
+            [5,    6,  96, 1, 1, 2],
+            [5,    6,  96, 1, 1, 1],
+            [5,    6,  96, 1, 1, 1],
         ]
-        super().__init__(layers=layers, **kwargs)
+        super().__init__(cfgs=cfgs, mode='small', **kwargs)
