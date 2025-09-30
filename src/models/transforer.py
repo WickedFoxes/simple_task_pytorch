666a55beda1a41ac25f47dc742466530b1ee5ce0
seokhun <sostar0832@cau.ac.kr>
2025-10-01 02:55:14 +0900

    transformer 추가

--- src/models/transforer.py
+++ src/models/transforer.py
@@ -22,21 +22,23 @@ from src.registry import register
 from src.models.base import ModelBase
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_embed, max_len=256, device=torch.device("cpu")):
+    def __init__(self, d_embed, max_len=256):
         super(PositionalEncoding, self).__init__()
         encoding = torch.zeros(max_len, d_embed)
-        encoding.requires_grad = False
         position = torch.arange(0, max_len).float().unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_embed, 2) * -(math.log(10000.0) / d_embed))
         encoding[:, 0::2] = torch.sin(position * div_term)
         encoding[:, 1::2] = torch.cos(position * div_term)
-        self.encoding = encoding.unsqueeze(0).to(device)
+
+        # register_buffer -> 학습 파라미터는 아니지만 디바이스 이동 가능
+        self.register_buffer("encoding", encoding.unsqueeze(0))
 
     def forward(self, x):
         _, seq_len, _ = x.size()
         pos_embed = self.encoding[:, :seq_len, :]
         out = x + pos_embed
         return out
+
 
 def scaled_dot_product_attention(
         query, 
