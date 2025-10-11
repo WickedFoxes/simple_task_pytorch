833e5c563033f7364bde34ebe9b689484b6f5422
seokhun <sostar0832@cau.ac.kr>
2025-10-11 13:20:43 +0900

    config 파일 정리

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,6 +5,6 @@ from src.models.lstm import LSTMClassifier
 from src.models.bert import BertClassifier
 from src.models.se_resnetv2 import SE_ResNet_mini_v2_28, SE_ResNet_mini_v2_14x2
 from src.models.mobile_v3 import mobilenet_v3_small, mobilenet_v3_large, mobilenet_v3_large_pretrained, efficientnet_b0_mini
-from src.models.efficientnet import EfficientNetB0, EfficientNetB0Pretrained, EfficientNet_mini_28, EfficientNet_mini_14x2, EfficientNet_mini
+from src.models.efficientnet import EfficientNetB0
 from src.models.transfomer import TransformerTL
 from src.models.vision_transformer import VisionTransformer
