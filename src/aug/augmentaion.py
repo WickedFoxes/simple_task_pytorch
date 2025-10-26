dc66e19e7e0bfe9b32dbfc22e1367456e5f0d370
seokhun <sostar0832@cau.ac.kr>
2025-10-26 16:28:09 +0900

    vit pretrained 추가

--- src/aug/augmentaion.py
+++ src/aug/augmentaion.py
@@ -1,10 +1,20 @@
 from torchvision import transforms
+from torchvision.transforms import InterpolationMode
+
+STR2INTERP = {
+    "nearest": InterpolationMode.NEAREST,
+    "bilinear": InterpolationMode.BILINEAR,
+    "bicubic": InterpolationMode.BICUBIC,
+    "box": InterpolationMode.BOX,
+    "hamming": InterpolationMode.HAMMING,
+    "lanczos": InterpolationMode.LANCZOS,
+}
 
 # 필요 시 Albumentations 도 같은 방식으로 추가 등록 가능
 _AUG_MAP = {
-  "Resize": lambda **p: transforms.Resize(p["size"], interpolation=p.get("interpolation", transforms.InterpolationMode.BILINEAR)),
+  "Resize": lambda **p: transforms.Resize(p["size"], interpolation=STR2INTERP[p.get("interpolation", "bilinear")]),
   "RandomCrop": lambda **p: transforms.RandomCrop(p["size"], padding=p.get("padding", 0)),
-  "RandomResizedCrop": lambda **p: transforms.RandomResizedCrop(p["size"], scale=p.get("scale", (0.08, 1.0)), ratio=p.get("ratio", (3./4., 4./3.)), interpolation=p.get("interpolation", transforms.InterpolationMode.BILINEAR)),
+  "RandomResizedCrop": lambda **p: transforms.RandomResizedCrop(p["size"], scale=p.get("scale", (0.08, 1.0)), ratio=p.get("ratio", (3./4., 4./3.)), interpolation=STR2INTERP[p.get("interpolation", "bilinear")]),
   "RandomHorizontalFlip": lambda **p: transforms.RandomHorizontalFlip(p.get("p", 0.5)),
   "RandAugment": lambda **p: transforms.RandAugment(num_ops=p.get("n",2), magnitude=p.get("m",9)),
   "ToTensor": lambda **p: transforms.ToTensor(),
